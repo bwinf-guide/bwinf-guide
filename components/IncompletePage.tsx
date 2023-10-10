2766cac6af8f924a754e892a1ab971b4046d2048
Chuyang Wang <mail@chuyang-wang.de>
2023-10-10 18:42:54 +0200

    content: contribution.mdx meta: rename allgemeines to general

--- components/IncompletePage.tsx
+++ components/IncompletePage.tsx
@@ -2,15 +2,15 @@ import { Callout } from "nextra/components";
 import React from "react";
 import config from "../theme.config"
 
-const IncompleteSection: React.FC = () => {
+const IncompletePage: React.FC = () => {
   return (
     <Callout type="warning">
       <p>
-        Dieser Abschnitt ist noch nicht fertig. Wenn du uns helfen möchtest, kannst
+        Diese Seite ist noch nicht fertig. Wenn du uns helfen möchtest, kannst
         du gerne auf Github <a target="_blank" href={config.project.link + "\\pulls"} className="font-bold text-amber-700 dark:text-amber-400 hover:text-amber-500 dark:hover:text-amber-300">ein Pull Request erstellen</a>. 
       </p>
     </Callout>
   );
 }
 
-export default IncompleteSection;
+export default IncompletePage;
