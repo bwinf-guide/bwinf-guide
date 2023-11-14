de376ad525f643d5bfd585b2c108ffe9e6100559
Chuyang Wang <mail@chuyang-wang.de>
2023-11-14 21:48:22 +0100

    content(index): change description

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,73 +1,88 @@
-import AppFooter from '@components/AppFooter'
-import React from 'react'
-import { ThemeSwitch, useConfig } from 'nextra-theme-docs'
-import { useRouter } from 'next/router'
+import AppFooter from "@components/AppFooter";
+import React from "react";
+import { ThemeSwitch, useConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
 
 export default {
-  logo: <span><strong>BWINF GUIDE</strong></span>,
+  logo: (
+    <span>
+      <strong>BWINF GUIDE</strong>
+    </span>
+  ),
   footer: {
-    component: AppFooter
+    component: AppFooter,
   },
   project: {
-    link: 'https://github.com/bwinf-guide/bwinf-guide'
+    link: "https://github.com/bwinf-guide/bwinf-guide",
   },
   docsRepositoryBase: "https://github.com/bwinf-guide/bwinf-guide/tree/main",
   useNextSeoProps() {
-    const { asPath } = useRouter()
-    if (asPath !== '/') {
+    const { asPath } = useRouter();
+    if (asPath !== "/") {
       return {
-        titleTemplate: '%s - BWINF GUIDE',
-      }
+        titleTemplate: "%s - BWINF GUIDE",
+      };
     }
   },
   head: () => {
-    const { asPath, defaultLocale, locale } = useRouter()
-    const { title, frontMatter } = useConfig()
-    const url =
-      'https://bwinf.guide' + asPath
-    
-    const robots = asPath == "/impressum" ? <meta name="robots" content="noindex, nofollow" /> : <></>;
- 
+    const { asPath, defaultLocale, locale } = useRouter();
+    const { title, frontMatter } = useConfig();
+    const url = "https://bwinf.guide" + asPath;
+
+    const robots =
+      asPath == "/impressum" ? (
+        <meta name="robots" content="noindex, nofollow" />
+      ) : (
+        <></>
+      );
+
     return (
       <>
         <meta property="og:url" content={url} />
         <meta property="og:site_name" content="BWINF.GUIDE"></meta>
         <meta
           name="og:title"
-          content={title ? title + ' - BWINF GUIDE' : 'BWINF GUIDE'}
+          content={title ? title + " - BWINF GUIDE" : "BWINF GUIDE"}
         />
         <meta
           property="og:description"
-          content={frontMatter.description || 'Eine Einführung in den Bundeswettbewerb Informatik'}
+          content={
+            frontMatter.description ||
+            "Der BWINF-GUIDE ist eine Sammlung von Themen, Tipps und Tricks für den Bundeswettbewerb Informatik, die euch einen leichteren Einstieg ermöglicht sowie Materialien zur Vorbereitung für alle Runden bereitstellt."
+          }
         />
-        { robots }
+        {robots}
       </>
-    )
+    );
   },
   primaryHue: 48,
   primarySaturation: 100,
   themeSwitch: {
     useOptions() {
       return {
-        light: 'Hell',
-        dark: 'Dunkel',
-        system: 'System'
-      }
-    }
+        light: "Hell",
+        dark: "Dunkel",
+        system: "System",
+      };
+    },
   },
   editLink: {
     text: "Diese Seite auf Github bearbeiten →",
   },
   feedback: {
-    content: "Fragen? Gib uns ein Feedback →"
+    content: "Fragen? Gib uns ein Feedback →",
   },
   search: {
-    placeholder: "Durchsuchen..."
+    placeholder: "Durchsuchen...",
   },
   navbar: {
-    extraContent: <><ThemeSwitch lite={true} /></>
+    extraContent: (
+      <>
+        <ThemeSwitch lite={true} />
+      </>
+    ),
   },
   toc: {
-    title: "Auf dieser Seite"
-  }
-}
+    title: "Auf dieser Seite",
+  },
+};
